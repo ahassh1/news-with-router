2f31550cb1a314136c2b4a009523b4a904a980dd
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-27 15:03:42 +0600

    create route and render

---
+++ routes/router.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Home Layout</h2>,
+  },
+  {
+    path: "/auth",
+    element: <h2>Authentication Layout</h2>,
+  },
+  {
+    path: "/",
+    element: <h2>Home</h2>,
+  },
+  {
+    path: "/",
+    element: <h2>Home</h2>,
+  },
+  {
+    path: "/",
+    element: <h2>Home</h2>,
+  },
+]);
+export default router;
