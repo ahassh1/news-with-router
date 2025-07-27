4d4f4ee075bb2a4f1ebdabc53def5edeb812258e
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-27 16:25:03 +0600

    color added in daisyui

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <img src="../src/assets/logo.png" alt="" />
+    <div className="flex flex-col justify-center items-center gap-3 pt-2">
+      <img className="w-[350px]" src="../src/assets/logo.png" alt="" />
       <p className="text-gray-400">
         Journalism wihout a fear and all about favour
       </p>
