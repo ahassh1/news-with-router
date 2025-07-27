33a849a5ea935ba57d2851aeb197cc7f024f80bb
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-27 14:21:19 +0600

    file initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="bg-red-500 text-2xl text-white"> hello react</h1>
+    </>
+  );
+}
+
+export default App;
