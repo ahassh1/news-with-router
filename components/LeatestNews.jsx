475dc395145f67005718cd5387b367ac6ce06664
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-27 20:24:45 +0600

    marquee added

--- components/LeatestNews.jsx
+++ components/LeatestNews.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const LeatestNews = () => {
   return (
     <div className="flex items-center gap-5 bg-base-200 p-2.5">
       <p className="text-base-100 bg-secondary px-3 py-2">leatest</p>
-      <p>
-        Now the milestone tradegy is going to upperhand on the talk of the town
-        for any quires of everyone shouldn't overwhelming this issues....
-      </p>
+      <Marquee className="flex gap-2.5 pauseOnHover={true} speed={50}">
+        <p className="font-semibold">
+          Now the milestone tradegy is going to upperhand on the talk of the
+          town for any quires of everyone shouldn't overwhelming this issues.
+        </p>
+        <p className="font-semibold">
+          Now the milestone tradegy is going to upperhand on the talk of the
+          town for any quires of everyone shouldn't overwhelming this issues.
+        </p>
+        <p className="font-semibold">
+          Now the milestone tradegy is going to upperhand on the talk of the
+          town for any quires of everyone shouldn't overwhelming this issues
+        </p>
+      </Marquee>
     </div>
   );
 };
