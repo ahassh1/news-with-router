d893a45f7bfb54e55d202d0aa9c59d42e4a38792
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-27 16:44:42 +0600

    time,date added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,15 @@
+import { format } from "date-fns";
 import React from "react";
 
 const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-3 pt-2">
       <img className="w-[350px]" src="../src/assets/logo.png" alt="" />
-      <p className="text-gray-400">
+      <p className="text-gray-600">
         Journalism wihout a fear and all about favour
+      </p>
+      <p className="text-gray-400">
+        {format(new Date(), "EEEE, MMMM MM , yyyy")}
       </p>
     </div>
   );
