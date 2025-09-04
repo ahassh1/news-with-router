fadf8226650c8e81d4e1f60b181a2921980d909a
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-04 14:33:01 +0600

    Qzone added

---
+++ components/homelayout/Qzone.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Qzone = () => {
+  return (
+    <div className="bg-base-200 p-3">
+      <h2 className="font-bold mb-5">QZone</h2>
+      <div className="space-y-5">
+        <img
+          src="https://fdn.gsmarena.com/imgroot/reviews/24/apple-iphone-16-pro-max/-728x314/gsmarena_002.jpg"
+          alt=""
+        />
+        <img
+          src="https://fdn.gsmarena.com/imgroot/reviews/24/honor-magic-v3/-728x314/gsmarena_000.jpg"
+          alt=""
+        />
+        <img
+          src="https://fdn.gsmarena.com/imgroot/reviews/24/xiaomi-14-ultra/-728x314/gsmarena_002.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Qzone;
--- components/homelayout/RightAside.jsx
+++ components/homelayout/RightAside.jsx
@@ -1,41 +1,16 @@
 import React from "react";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import SocialLogin from "./SocialLogin";
+import FindUs from "./FindUs";
+import Qzone from "./Qzone";
 
-const FindUs = () => {
+const RighAside = () => {
   return (
-    <div>
-      <h2 className="font-bold mb-5">Find Us on</h2>
-      <div className="join join-vertical w-full">
-        <a
-          href="https://facebook.com/yourpage"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn bg-base-100 justify-start join-item"
-          title="Visit us on Facebook"
-        >
-          <FaFacebook className="mr-2" /> Facebook
-        </a>
-        <a
-          href="https://twitter.com/yourpage"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn bg-base-100 justify-start join-item"
-          title="Visit us on Twitter"
-        >
-          <FaTwitter className="mr-2" /> Twitter
-        </a>
-        <a
-          href="https://instagram.com/yourpage"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn bg-base-100 justify-start join-item"
-          title="Visit us on Instagram"
-        >
-          <FaInstagram className="mr-2" /> Instagram
-        </a>
-      </div>
+    <div className="">
+      <SocialLogin></SocialLogin>
+      <FindUs></FindUs>
+      <Qzone></Qzone>
     </div>
   );
 };
 
-export default FindUs;
+export default RighAside;
