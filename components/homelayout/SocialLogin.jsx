317a81ff48463ac3a8433205d30e727df3933bb5
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-04 12:43:09 +0600

    useeffect added in condition rendaring

--- components/homelayout/SocialLogin.jsx
+++ components/homelayout/SocialLogin.jsx
@@ -6,12 +6,12 @@ const SocialLogin = () => {
   return (
     <div>
       <h2 className="font-bold">Login with</h2>
-      <div className="space-y-2 md:space-y-3">
-        <button className="btn btn-outline w-full btn-secondary">
+      <div className="space-y-3 md:space-y-3">
+        <button className="btn btn-outline w-full btn-secondary text-[8px] md:text-[14px]">
           {" "}
           <FcGoogle size={24} /> Login with google
         </button>
-        <button className="btn btn-outline btn-primary w-full">
+        <button className="btn btn-outline btn-primary  text-[8px] md:text-[14px] w-full">
           <FaGithub size={24} /> Login with github
         </button>
       </div>
--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -9,9 +9,19 @@ const CategoryNews = () => {
 
   // console.log(id, data);
   useEffect(() => {
-    const filteredNews = data.filter((news) => news.category_id == id);
-    console.log(filteredNews);
-    setCategotyNews(filteredNews);
+    if (id == "0") {
+      setCategotyNews(data);
+      return;
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategotyNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      console.log(filteredNews);
+      setCategotyNews(filteredNews);
+    }
   }, [data, id]);
   return <div>Total -{categoryNews.length} Found</div>;
 };
