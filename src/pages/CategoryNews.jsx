57f7bf1ea139b7cd3f45c5a08f0b611aadf3c33a
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-04 12:19:21 +0600

    usestate added

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
   const { id } = useParams();
   const data = useLoaderData();
+
+  const [categoryNews, setCategotyNews] = useState([]);
+
   // console.log(id, data);
   useEffect(() => {
     const filteredNews = data.filter((news) => news.category_id == id);
     console.log(filteredNews);
+    setCategotyNews(filteredNews);
   }, [data, id]);
-  return <div>category news - {id}</div>;
+  return <div>Total -{categoryNews.length} Found</div>;
 };
 
 export default CategoryNews;
